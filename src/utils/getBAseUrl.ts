1f7c29094d4b8f81dcb98f52467cc4899567e804
wangye <jiangyiduo@192.168.1.34>
2025-03-05 19:40:47 +0800

    a

--- src/utils/getBAseUrl.ts
+++ src/utils/getBAseUrl.ts
@@ -2,8 +2,8 @@ const getBaseUrl = () => {
     let BASE_URL = '';
     if (process.env.NODE_ENV === 'development') {
       //开发环境 - 根据请求不同返回不同的BASE_URL
-    // BASE_URL = 'http://smgiji.cloud.natool.cn'
-    BASE_URL = 'http://60.205.91.224:8080'
+    BASE_URL = 'http://aoltps.demo.natool.cn'
+    // BASE_URL = 'http://60.205.91.224:8080'
     // BASE_URL = 'http://fsdyt.cn'
     } else {
       // 生产环境
